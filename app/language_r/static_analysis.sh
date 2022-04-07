07482cdf918195c6280b659bd3a5e09d6ac341bb
jwons <joe.wonsil@gmail.com>
2022-04-07 14:24:41 -0700

    bug fix when there are spaces in the R directory names

--- app/language_r/static_analysis.sh
+++ app/language_r/static_analysis.sh
@@ -4,4 +4,4 @@ static_analysis_dir="$2"
 # suppress R output by redirecting to /dev/null
 echo "Attempting to run static analysis for raw R scripts in dataset..."
 Rscript --default-packages=methods,datasets,utils,grDevices,graphics,stats \
-	app/language_r/static_analysis.R $doi_direct $static_analysis_dir
+	app/language_r/static_analysis.R "$doi_direct" $static_analysis_dir
